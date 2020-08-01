3e94ea5877dd2ed98566f4e943733079a17430f3
OnePlusZero <54687284+OnePlusZero@users.noreply.github.com>
2020-08-02 00:26:14 +0300

    update mobile design

--- corka/corka/settings.py
+++ corka/corka/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w7)k4&oae=cp#(g36wbbojdmq%$!h7i!@x7$ghrs_w7$%bh!5p'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.1.100']
 
 
 # Application definition

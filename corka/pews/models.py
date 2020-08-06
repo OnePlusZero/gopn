f8e78c050ebe231c9ca81dd19f002a1b2c97663a
OnePlusZero <54687284+OnePlusZero@users.noreply.github.com>
2020-08-06 09:30:58 +0300

    new base

--- corka/pews/models.py
+++ corka/pews/models.py
@@ -5,7 +5,10 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, verbose_name="Автор")
     title = models.CharField(max_length=200,verbose_name="Заголовок")
-    text = models.TextField(verbose_name="Текст")
+    text = models.TextField(verbose_name="Текст пiдзаголовка")
+    textcontent = models.TextField(verbose_name="Текст новини", null=True)
+    # slug - поле для формирования
+    slug = models.SlugField(max_length=250,unique_for_date='published_date')
 
     created_date = models.DateTimeField(
             default=timezone.now,verbose_name="дата створення")

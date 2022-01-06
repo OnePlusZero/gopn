201a74b2bdfeecec8278c729fccf8ebd359fbf5a
OnePlusZero <dimasfanzerg1@outlook.com>
2022-01-06 10:22:58 +0200

    Fix to django 3.2

--- corka/corka/urls.py
+++ corka/corka/urls.py
@@ -14,14 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import re_path, include
+from django.urls import re_path, include, path
 from django.conf.urls import include,  static
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls import url
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    re_path('', include('pews.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('pews.urls')),
 
 ]
 
--- corka/pews/urls.py
+++ corka/pews/urls.py
@@ -1,22 +1,22 @@
-from django.urls import re_path
+from django.urls import path
 from .views import position_list, post_list, about, gal, howtous, test, doc, news, struktura, post_detail, zak, post_details
-from django.conf.urls import include, static
+from django.conf.urls import include, static, url
 from django.conf import settings
 
 
 urlpatterns = [
-    re_path('', post_list, name='post_list'),
-    re_path('redux', position_list, name='position_list'),
-    re_path('about', about, name='about'),
-    re_path('gal', gal, name='gal'),
-    re_path('howtous', howtous, name='howtous'),
-    re_path('test', test, name='test'),
-    re_path('doc', doc, name='doc'),
-    re_path('news', news, name='news'),
-    re_path('struktura', struktura, name='struktura'),
-    re_path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-            post_detail, name='post_detail'),
-    re_path('zak', zak, name='zak'),
+    path('', post_list, name='post_list'),
+    path('redux', position_list, name='position_list'),
+    path('about', about, name='about'),
+    path('gal', gal, name='gal'),
+    path('howtous', howtous, name='howtous'),
+    path('test', test, name='test'),
+    path('doc', doc, name='doc'),
+    path('news', news, name='news'),
+    path('struktura', struktura, name='struktura'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         post_detail, name='post_detail'),
+    path('zak', zak, name='zak'),
 
 
 ]
